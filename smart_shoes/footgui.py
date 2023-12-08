8719bbf8558fd377bfeb33ee0cd8443f5b6f3ae9
MISTER_NEGATIVE <supergogetavegito21@gmail.com>
2023-12-08 13:17:26 +0530

    Update footgui.py

--- smart_shoes/footgui.py
+++ smart_shoes/footgui.py
@@ -18,37 +18,41 @@ circle_positions = [
 # Establish serial connection
 ser = serial.Serial('COM5', 115200)  # Replace 'COM5' with your COM port
 
-def plot_circle(x, y, color, alpha):
-    circle = plt.Circle((x, y), radius=10, color=color, alpha=alpha)
+def get_color(value):
+    # Map sensor value to color range
+    if value <= 50:
+        return (1, 0, 0)  # Red for lower values
+    elif value >= 400:
+        return (0, 0, 1)  # Blue for higher values
+    else:
+        normalized_value = (value - 50) / 350  # Normalize value between 50 and 400
+        return (1 - normalized_value, normalized_value, 0)  # Vary between red and green for intermediate values
+
+def plot_circle(x, y, value):
+    color = get_color(value)
+    circle = plt.Circle((x, y), radius=10, color=color, alpha=0.7)
     ax.add_patch(circle)
 
 while True:
     try:
+        # Clear the previous circles for the next data update
+        ax.clear()
+        imgplot = ax.imshow(foot_img)
+
         data = ser.readline().decode().strip()
         sensor_data = json.loads(data)  # Decode JSON data
-
-        # Map sensor IDs to plot functions
-        plot_functions = {
-            'sensor1': lambda value: plot_circle(circle_positions[0][0], circle_positions[0][1], 'red', 0.5) if value <= 20 else None,
-            'sensor2': lambda value: plot_circle(circle_positions[1][0], circle_positions[1][1], 'orange', 0.6) if value <= 100 else None,
-            'sensor3': lambda value: plot_circle(circle_positions[2][0], circle_positions[2][1], 'green', 0.7) if value <= 200 else None,
-            'sensor4': lambda value: plot_circle(circle_positions[3][0], circle_positions[3][1], 'yellow', 0.8) if value <= 300 else None,
-            'sensor5': lambda value: plot_circle(circle_positions[4][0], circle_positions[4][1], 'blue', 0.9) if value <= 400 else None
-        }
 
         # Plot sensor data on the foot image
         for sensor_id, value in sensor_data.items():
             value = int(value)
-            if sensor_id in plot_functions:
-                plot_functions[sensor_id](value)
+            sensor_number = int(sensor_id[6:]) - 1  # Extract sensor number from 'sensorX'
+            if 0 <= sensor_number < len(circle_positions):
+                x, y = circle_positions[sensor_number]  # Get circle position
+                plot_circle(x, y, value)
 
         # Refresh the plot
         fig.canvas.draw()
         plt.pause(0.1)  # Adjust the pause duration as needed
-
-        # Clear the previous circles for the next data update
-        ax.clear()
-        imgplot = ax.imshow(foot_img)
 
     except Exception as e:
         print("Error:", e)
